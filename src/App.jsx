f08af37fa498f612a8d87c19103e95dd983785c3
axa000 <raymond874100@gmail.com>
2022-03-27 18:18:51 -0700

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Header from './components/header/header'
+import Nav from './components/nav/nav'
+import About from './components/about/About'
+import Experience from './components/experience/experience'
+
+import Portfolio from './components/portfolio/portfolio'
+import Contact from './components/contact/contact'
+import Footer from './components/footer/footer'
+
+const index = () => {
+  return (
+    <>
+    
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+
+      <Portfolio />
+      <Contact />
+      <Footer />
+
+
+    </>
+  )
+}
+
+export default index
+//       <Services />
