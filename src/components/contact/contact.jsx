97932adfecb4d80da7df502266026e601f0b733b
axa000 <raymond874100@gmail.com>
2022-03-29 17:20:13 -0700

    completed portfolio, experience, and contact

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
 import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
 
 const contact = () => {
   return (
-    <section id='contact'>contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className='contact__option'>
+            <MdOutlineEmail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <small>axa00000@uw.edu</small>
+            <a href="mailto:axa00000@uw.edu">Send a Message</a>
+          </article>
+
+          <article className='contact__option'>
+            <MdOutlineEmail className='contact__option-icon'/>
+            <h4>Telegram</h4>
+            <small>+9087238580</small>
+            <a href="https://api.telegram.org/sendMessage?chat_id=719592907">Send a Message</a>
+          </article>
+        </div>
+
+        <form action="">
+          <input type="text" name='name' placeholder='Your Full Name' required/>
+          <input type="email" email = 'email' placeholder='Your Email' required/>
+          <textarea name="message" rows = "7" placeholder='Your Message' required ></textarea>
+          <button type = 'submit' className='btn btn-primary'>Send Message</button>
+        </form>
+
+      </div>
+    </section>
   )
 }
 
--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -1,9 +1,86 @@
 import React from 'react'
 import './experience.css'
+import {BsFillPatchCheckFill} from 'react-icons/bs'
+
 
 const experience = () => {
   return (
-    <section id='experience'>experience</section>
+    <section id='experience'>
+      <h5>What Skills I have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Programming Language</h3>
+          <div className="experience__content">
+             <article className="experience__details">
+               <BsFillPatchCheckFill className="experience__details-icon"/>
+               <div>
+                <h4>Python</h4>
+                <small className='text-light'>Experienced</small>
+               </div>
+             </article>
+             <article className="experience__details">
+               <BsFillPatchCheckFill className="experience__details-icon"/>
+               <div>
+                <h4>Java</h4>
+                <small className='text-light'>Experienced</small>
+               </div>
+             </article>
+             <article className="experience__details">
+               <BsFillPatchCheckFill className="experience__details-icon"/>
+               <div>
+                <h4>JavaScript</h4>
+                <small className='text-light'>Experienced</small>
+               </div>
+             </article>
+             <article className="experience__details">
+               <BsFillPatchCheckFill className="experience__details-icon"/>
+               <div>
+                <h4>C#</h4>
+                <small className='text-light'>Experienced</small>
+               </div>
+             </article>
+
+          </div>
+        </div>
+
+        <div className="experience__backend">
+          <h3>Database & Tech</h3>
+          <div className="experience__content">
+             <article className="experience__details">
+               <BsFillPatchCheckFill className="experience__details-icon"/>
+               <div>
+                <h4>Git</h4>
+                <small className='text-light'>Experienced</small>
+               </div>
+             </article>
+             <article className="experience__details">
+               <BsFillPatchCheckFill className="experience__details-icon"/>
+               <div>
+                <h4>Node.js</h4>
+                <small className='text-light'>Experienced</small>
+               </div>
+             </article>
+             <article className="experience__details">
+               <BsFillPatchCheckFill className="experience__details-icon"/>
+               <div>
+                <h4>SQL Server</h4>
+                <small className='text-light'>Experienced</small>
+               </div>
+             </article>
+             <article className="experience__details">
+               <BsFillPatchCheckFill className="experience__details-icon"/>
+               <div>
+                <h4>Restful API</h4>
+                <small className='text-light'>Experienced</small>
+               </div>
+             </article>
+
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
