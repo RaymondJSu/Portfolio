ea71dd97c8c511c8d3265c4de8245e8e85bd921d
RaymondJSu <raymond874100@gmail.com>
2024-09-17 16:39:44 -0700

    update podcast summarizer AI project

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -3,22 +3,29 @@ import './portfolio.css'
 import IMG1 from '../../assets/sorting-visualizer.PNG'
 import IMG2 from '../../assets/KOL.PNG'
 import IMG3 from '../../assets/h-m.PNG'
+import IMG4 from '../../assets/podcast.jpg'
 
 const data = [
   {
     id: 1,
+    image: IMG4,
+    title: 'Podcast Summarizer.AI',
+    github: 'https://podcast-summarizer-frontend.onrender.com/'
+  },
+  {
+    id: 2,
     image: IMG1,
     title: 'Sorting Visualizer',
     github: 'https://raymondjsu.github.io/sorting-visualizer/'
   },
   {
-    id: 2,
+    id: 3,
     image: IMG2,
     title: 'Reddit Key Opinion Leader',
     github: 'https://github.com/RaymondJSu/Reddit-key-opinion-leader'
   },
   {
-    id: 3,
+    id: 4,
     image: IMG3,
     title: 'H&M Personalized Fashion Recommendation',
     github: 'https://github.com/RaymondJSu/HM-personalized-fashion-recommendation'
